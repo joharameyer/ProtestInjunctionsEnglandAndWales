faec49f61f74b241e4497069791d96954061dd10
joharameyer <83025393+joharameyer@users.noreply.github.com>
2025-11-18 19:22:16 +0000

    Create popup-fix.js

---
+++ popup-fix.js
@@ -0,0 +1,10 @@
+var style = document.createElement('style');
+style.innerHTML = `
+.leaflet-popup-content {
+    font-size: 18px !important;
+    line-height: 1.4 !important;
+}
+.leaflet-popup-content-wrapper {
+    max-width: 90vw !important;
+}`;
+document.head.appendChild(style);
