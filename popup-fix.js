56cb941864ac20bf656433932134eacecfaff492
joharameyer <83025393+joharameyer@users.noreply.github.com>
2025-11-18 19:24:13 +0000

    Update popup-fix.js

--- popup-fix.js
+++ popup-fix.js
@@ -1,10 +1,12 @@
-var style = document.createElement('style');
-style.innerHTML = `
-.leaflet-popup-content {
-    font-size: 18px !important;
-    line-height: 1.4 !important;
-}
-.leaflet-popup-content-wrapper {
-    max-width: 90vw !important;
-}`;
-document.head.appendChild(style);
+window.addEventListener('load', function() {
+    var style = document.createElement('style');
+    style.innerHTML = `
+    .leaflet-popup-content {
+        font-size: 18px !important;
+        line-height: 1.4 !important;
+    }
+    .leaflet-popup-content-wrapper {
+        max-width: 90vw !important;
+    }`;
+    document.head.appendChild(style);
+});
